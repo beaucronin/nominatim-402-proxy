bfb6892b2d5bea5479f527a5d602a9b3216f6ca5
Beau Cronin <beau@21.co>
2016-05-22 09:41:11 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,31 @@
+import requests
+
+from flask import Flask, request, Response
+from werkzeug.datastructures import Headers
+
+from two1.wallet import Wallet
+from two1.bitserv.flask import Payment
+
+NOMINATIM_HOST = 'ec2-52-40-116-115.us-west-2.compute.amazonaws.com'
+NOMINATIM_PORT = '80'
+
+app = Flask(__name__)
+
+wallet = Wallet()
+payment = Payment(app, wallet)
+
+def do_call(method_name):
+    url = 'http://{}:{}/{}.php'.format(NOMINATIM_HOST, NOMINATIM_PORT, method_name)
+    r = requests.get(
+        url=url,
+        params=request.args if request.args else None)
+    headers = list(r.headers.items())
+    return Response(
+        r.text if r.text else None,
+        status = r.status_code,
+        headers = headers)
+
+@app.route('/reverse')
+@payment.required(5)
+def reverse():
+    return do_call('reverse')
